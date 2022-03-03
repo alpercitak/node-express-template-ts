7ee4f164a87b617097b1fbb5928f2d697243bedd
Alper Çıtak <citak.alper@gmail.com>
2022-03-03 12:29:50 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction, Application } from 'express';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 8000;
+const NAME = process.env.NAME || "";
+
+const app: Application = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use('/assets', express.static('public/assets/'));
+app.use(express.static('public/'));
+
+app.use((req: Request, res: Response, next: NextFunction): void => {
+    res.locals.title = process.env.NAME_READABLE;
+    return next();
+});
+app.get("/", (req: Request, res: Response): void => {
+    return res.render("home/index");
+});
+
+app.listen(PORT, (): void => {
+    console.log(`${NAME} started on ${new Date(Date.now()).toISOString()} | port:${PORT}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const util = require('util');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const config = {
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.m?js$/,
+                // exclude: /(node_modules|bower_components)/,
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-transform-runtime']
+                }
+            },
+            {
+                test: /\.less|\.css$/,
+                use: [
+                    // {
+                    //     loader: 'style-loader',
+                    //     options: {injectType: "styleTag"}
+                    // },
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            lessOptions: {
+                                strictMath: true,
+                            },
+                        },
+                    },
+
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            "jquery": path.join(__dirname, "./public/lib/jquery.stub/main.js")
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    watch: true,
+    watchOptions: {
+        ignored: ['node_modules/**']
+    },
+    mode: "production"
+};
+
+const exportables = [];
+const items = [
+    {name: "app", path: "views/__shared/app.js"},
+    {name: "modules/home", path: "views/home/index.js"},
+];
+items.map((x) => {
+    exportables.push({
+        ...config,
+        name: x.name,
+        entry: {[x.name]: path.join(__dirname, x.path)},
+        output: {path: path.join(__dirname, "public/assets"), filename: x.name + ".min.js"}
+    });
+});
+
+module.exports = [
+    ...exportables
+];
